3394b2cf64d308a193b880a21b6c701f07add7bd
xaviguasch <xaviguasch@gmail.com>
2019-04-16 20:35:02 +0200

    added ES8 solution

--- 08-ES8/exerciseES8-SOLUTIONS.js
+++ 08-ES8/exerciseES8-SOLUTIONS.js
@@ -16,16 +16,26 @@ console.log(turtle);
 console.log(rabbit);
 
 
+turtle = turtle.padStart(8);
+rabbit = rabbit.padStart(8);
+
+
 // #2) What happens when you run turtle.trim().padEnd(9, '=') on the turtle variable
 // Read about what the second parameter does in padEnd and padStart
 turtle = turtle.trim().padEnd(9, '=');
 
+'     ||<- Start line'
+'🐢======='
+'       🐇'
+
 
 // #3) Get the below object to go from:
 let obj = {
-    my: 'name',
-    is: 'Rudolf',
-    the: 'raindeer'
+  my: 'name',
+  is: 'Rudolf',
+  the: 'raindeer'
 }
 // to this:
 'my name is Rudolf the raindeer'
+
+Object.entries(obj).map(value => value.join(" ")).join(' ')
